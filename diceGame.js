f923113a40a382f799fb497f8c6f49ed2c957f00
Andrew Szymanek <andrewszymanek@Andrews-MacBook-Pro.local>
2019-10-16 08:46:13 -0500

    MVP before buttons added

--- diceGame.js
+++ diceGame.js
@@ -8,30 +8,34 @@
 // Bonus:
 // As a user, I want to see a visual progression up a "Mountain".
 
-climbMtEverest()
+
 function climbMtEverest(){
+    let stamina = 100;
     let health = 100;
+    let outcomesStamina = [-20, +10, -20, -10, +40, 0, -30, -100];
     let outcomesHealth = [-10, +5, -30, -20, +10, -20, -40, -100]; 
-    let outcome = ["Continue but low on food", "Received care package", "Illness", "Injury", "Rested-continue on!", "Frostbite", "Losing stamina", "Avalanche"]
-    alert("Your goal is to climb Mt. Everest to its summit to win. Your first step is to trek in to base camp. Click OK to roll dice and determine your success. As long as your health is over 0, you will reach the summit.")
+    let outcome = ["Low on food", "Received care package", "Illness", "Injury", "Rested-continue on!", "Frostbite", "Losing stamina", "Avalanche"]
+    alert("Your goal is to climb Mt. Everest to its summit to win. Your first step is to trek in to base camp. Click OK to roll dice and determine your success. As long as your health AND stamina are over 0, you will reach the summit.")
     let climbStage = ["Trek In", "Base Camp", "Camp 1" , "Camp 2", "Camp 3", "Summit Final Bid"];
     for(let i = 3; i <= 8; i ++){
         let diceRoll = rollDie(i); 
-        if(health <= 0){
+        if(health <= 0 && stamina <= 0){
+            stamina += outcomesStamina[diceRoll - 1];
             health += outcomesHealth[diceRoll - 1];
             { break; }
         }
-        else if(health > 0){
+        else if(health > 0 && stamina > 0){
+            stamina += outcomesStamina[diceRoll - 1];
             health += outcomesHealth[diceRoll - 1];
             
-        alert("You rolled " + diceRoll + "." + " " + outcome[diceRoll - 1] + " " + "at" + " " +  climbStage[i - 3] + "." + " " + "Current player health:" + " " + health);
+        console.log("You rolled " + diceRoll + "." + " " + outcome[diceRoll - 1] + " " + "at" + " " +  climbStage[i - 3] + "." + " " + "Current player health:" + " " + health + " " + "and" + " " + "current player stamina:" + " " + stamina + ".");
         } 
     }
-    if(health > 0){
-        alert("You made it to the top!");
+    if(health > 0 && stamina > 0){
+        console.log("You made it to the top!");
     }
     else{
-        alert("Try again!")
+        console.log("Try again!")
     } 
 }
 
