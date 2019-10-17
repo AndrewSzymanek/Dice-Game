354daaad45863494f5f4526f8778a89c31b2aede
Andrew Szymanek <andrewszymanek@Andrews-MacBook-Pro.local>
2019-10-17 07:16:18 -0500

    use this one

--- diceGame.js
+++ diceGame.js
@@ -1,12 +1,5 @@
 "use strict"
 
-//As a user, I want to climb a mountain where my success is based off the results of dice rolls.
-// As a user, I want the different sections of my climb to be based off different sided dice.
-// As a user, I want to have my health and stamina tracked as I climb where various events effect both.
-// As a user, I want to win the game, if I make it to the summit of the mountain with health and stamina greater than zero.
-// As a user, I want the ability to rest at the half way point which could restore my stamina and maybe some health.
-// Bonus:
-// As a user, I want to see a visual progression up a "Mountain".
 
 let stamina = 100;
 let health = 100;
